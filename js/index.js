2d93064c043943e8cfdba5ebd1f123119bca5960
Nurbaki Vural <nurbakivural@gmail.com>
2017-08-20 22:22:12 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,1 @@
-//Check out more cool stuff on my site: http://f-rilling.com/
-//if some fonts are not being loaded, reload the page
-//all fonts hosted on google fonts: https://www.google.com/fonts
+
